6095f054f162a5ab2041f0a66fc7180c9b671dab
Atharva <developer.atharva2001@gmail.com>
2025-03-13 19:58:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -47,5 +47,6 @@ def index():
         
     return render_template('index.html', recommended_books=recommended_books)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))  # Railway assigns a dynamic port
+    app.run(host="0.0.0.0", port=port, debug=True)
